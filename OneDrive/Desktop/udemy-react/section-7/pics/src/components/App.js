d383c87b86408c8d752cd6e322cf130990b1abaf
vicky <vcmartin2020@gmail.com>
2021-06-02 18:40:47 -0500

    ready for deploy

---
+++ OneDrive/Desktop/udemy-react/section-7/pics/src/components/App.js
@@ -0,0 +1,29 @@
+//Its convention to put packages we add to our app up at the top
+import React from 'react'
+import unsplash from '../api/unsplash'
+import SearchBar from './SearchBar'
+import ImageList from './ImageList'
+
+
+class App  extends React.Component{
+    state = { images: [] };
+
+    onSearchSubmit = async term => {
+        const response = await unsplash.get('/search/photos',{
+            params: {query: term}, //this params adds to the end of the search/photos/RIGHT HERE
+        });
+
+        this.setState({images: response.data.results });
+    }
+
+    render(){
+        return (
+            <div className="ui container" style={{ marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+                <ImageList images={this.state.images}/>
+            </div>
+        );
+    }
+}
+
+export default App
